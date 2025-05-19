9f3091e723898d4f9424a346b4cebb2508633b0a
zAstrvl <xrean.aep@gmail.com>
2025-05-19 11:40:53 +0300

    third commit

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,7 +1,34 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Moon, Sun} from "lucide-react";
 
 export const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    return <button>{isDarkMode ? <Sun /> : <Moon />}</button>
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme === "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.add("dark");
+        }
+        else {
+            setIsDarkMode(false);
+            document.documentElement.classList.remove("dark");
+        }
+    }, [])
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            setIsDarkMode(false);
+        }
+        else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+            setIsDarkMode(true);
+        }
+    }
+    return(
+        <button onClick={toggleTheme}>
+            {" "} {isDarkMode ? <Sun className="h-6 w-6 text-yellow-300" /> : <Moon className="h-6 w-6 text-blue-900" />}</button>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
+import {ThemeToggle} from "@/components/ThemeToggle.jsx";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
         {/* Tema */}
-
+        <ThemeToggle />
         {/* Arkaplan Efektleri */}
 
         {/* NavBar */}
